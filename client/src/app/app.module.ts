00f6d24809b1d73c73a9b31faaae5f486abb70aa
Jordan Powers <jordanpowers1227@gmail.com>
2023-02-20 15:04:27 -0800

    Add config page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,22 +5,37 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ConfigComponent } from './components/config/config.component';
+import { SpinnerComponent } from './components/reuse/spinner/spinner.component';
+
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfigComponent
+    ConfigComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/config/config.component.ts
+++ client/src/app/components/config/config.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, combineLatest, map, Observable, ReplaySubject } from 'rxjs';
+import { Configuration } from 'src/app/models/configuration.model';
+import { RobotId } from 'src/app/models/remote.model';
+import { ConfigService } from 'src/app/services/config.service';
+import { RemoteService } from 'src/app/services/remote.service';
+
+enum PageState {
+  SET_NAME,
+  LOAD_RID,
+  SHOW_CONFIG
+};
 
 @Component({
   selector: 'app-config',
@@ -6,5 +18,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./config.component.scss']
 })
 export class ConfigComponent {
+  protected currConfig = new ReplaySubject<Configuration>(1);
+  protected state: BehaviorSubject<PageState>;
+  protected stateType = PageState;
 
+  protected nameForm;
+
+  constructor(
+    private configService: ConfigService,
+    private remoteService: RemoteService
+  ) {
+    const config = configService.getConfig();
+
+    let state = PageState.SET_NAME;
+    let name = '';
+    if(config) {
+      name = config.name;
+      this.currConfig.next(config);
+      state = PageState.SHOW_CONFIG;
+    }
+
+    this.state = new BehaviorSubject<PageState>(state);
+
+    this.nameForm = new FormGroup({
+      name: new FormControl(name, Validators.required)
+    });
+
+    this.currConfig.subscribe(configService.setConfig);
+  }
+
+  protected updateName() {
+    const nameValue = this.nameForm.value.name;
+    if(!this.nameForm.valid || !nameValue) {
+      return;
+    }
+    this.state.next(PageState.LOAD_RID);
+
+    this.remoteService.getRobotAssignment(nameValue).subscribe((rid) => {
+      this.state.next(PageState.SHOW_CONFIG);
+      this.currConfig.next({
+        name: nameValue!!,
+        robotId: rid.robot_number
+      });
+    })
+  }
+
+  protected resetConfig() {
+    this.configService.clearConfig();
+    this.state.next(PageState.SET_NAME);
+  }
 }
