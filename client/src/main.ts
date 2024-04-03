47cd3728ae776518fc37ca15864b723d0c1d26a1
Jordan Powers <jordanpowers1227@gmail.com>
2024-04-03 15:58:57 -0700

    Redirect right-clicks to left-clicks
    Fixes #8

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,3 +5,9 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+window.oncontextmenu = function(event: MouseEvent) {
+  event.preventDefault();
+  const element = document.elementFromPoint(event.x, event.y) as any;
+  element.click();
+}
